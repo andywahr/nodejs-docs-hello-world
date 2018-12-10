8accf46cca8aa1def036f453ef85e2fd15ec06e2
Andy Wahrenberger <andywahr@microsoft.com>
2018-12-10 09:59:15 -0600

    add App insights

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var http = require('http');
+const appInsights = require("applicationinsights");
+appInsights.setup(process.env['AppInsightsKey']);
+appInsights.start();
 
 var server = http.createServer(function(request, response) {
-
+    appInsights.defaultClient.trackNodeHttpRequest({request: request, response: response});
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
