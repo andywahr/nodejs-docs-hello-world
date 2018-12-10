d88587f2703c385c1212e016e80d30e8965d07dd
Andy Wahrenberger <andywahr@microsoft.com>
2018-12-10 14:26:05 -0600

    no more app insights

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var http = require('http');
-const appInsights = require("applicationinsights");
-appInsights.setup(process.env['APPINSIGHTS_INSTRUMENTATIONKEY']);
-appInsights.start();
+//const appInsights = require("applicationinsights");
+//appInsights.setup(process.env['APPINSIGHTS_INSTRUMENTATIONKEY']);
+//appInsights.start();
 
 var server = http.createServer(function(request, response) {
     console.log("Request Made");
-    appInsights.defaultClient.trackNodeHttpRequest({request: request, response: response});
+    //appInsights.defaultClient.trackNodeHttpRequest({request: request, response: response});
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
